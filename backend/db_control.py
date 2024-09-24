652fa1dd19cf1d74ed9a30404f6f073724c4e293
Juhun Park <riann3207@gmail.com>
2024-09-24 18:54:33 -0400

    -

--- backend/db_control.py
+++ backend/db_control.py
@@ -16,6 +16,27 @@ class db:
     port = os.getenv("DB_PORT", "5432")
     database = os.getenv("DB_NAME")
 
+    session_meeting_key = {
+        "Bahrain Grand Prix" : 1229,
+        "Saudi Arabian Grand Prix" : 1230,
+        "Australian Grand Prix" : 1231,
+        "Japanese Grand Prix" : 1232,
+        "Chinese Grand Prix" : 1233,
+        "Miami Grand Prix" : 1234,
+        "Emilia Romagna Grand Prix" : 1235,
+        "Monaco Grand Prix" : 1236,
+        "Canadian Grand Prix" : 1237,
+        "Spanish Grand Prix" : 1238,
+        "Austrian Grand Prix" : 1239,
+        "British Grand Prix" : 1240,
+        "Hungarian Grand Prix" : 1241,
+        "Belgian Grand Prix" : 1242,
+        "Dutch Grand Prix" : 1243,
+        "Italian Grand Prix" : 1244,
+        "Azerbaijan Grand Prix" : 1245,
+        "Singapore Grand Prix" : 1246
+    }
+
     def db_commit_sessions(self):
 
         # OpenF1 API
